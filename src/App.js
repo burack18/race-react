22abf004d14df906920297b5e83d81248694bef3
burack <omerfaruk.aksak@student.ucll.be>
2023-02-02 00:17:29 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const initArr=new Array(50).fill({}).map(item=>{return {x:Math.floor(Math.random()*500),y:0}})
+console.log(initArr);
+
 function App() {
+  const [rain, setRain] = useState(initArr)
+  const [max, setMax] = useState(25);
+
+  useEffect(() => {
+    const inter=setInterval(() => {
+   
+      setRain(prev=>{
+        const m =Math.max(...prev.map(item=>item.y));        
+        setMax(pre=>m+25);
+        if(m+25>898){
+          clearInterval(inter)
+        }
+        return prev.map(item=>{return{x:item.x,y:item.y+Math.floor(Math.random()*3)
+        }})})
+        
+    }, 50);
+    
+  }, [])
+  
+  
+  const createTearStyle=(x,y)=>{
+    return {position:'relative',top:y,left:x,width:'5px',height:'25px',backgroundColor:max>100&&y>max-26?'red':'green'};
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className="App" style={{position:'fixed',display:'flex'}}>
+      {rain.map(t=><div style={createTearStyle(t.x,t.y)}></div>)}
     </div>
+      {<div style={{position:'relative',top:`${max}px`,width:'100vh',height:'2px',backgroundColor:'black'}}><p>Distance:{900-max}</p></div>}
+      {<div style={{position:'relative',top:`900px`,width:'100vh',height:'2px',borderTopStyle:'dashed'}}/>}
+
+  </>
   );
 }
 
